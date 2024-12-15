47237b60b0843fe55bfdfb1cb5ca3eb1866df5da
Khanh <knh.nguyen2002@gmail.com>
2024-12-15 07:49:03 -0600

    Added cpp to treesitter.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
 	config = function()
 		local config = require("nvim-treesitter.configs")
 		config.setup({
-			ensure_installed = { "lua", "javascript", "c", "html" },
+			ensure_installed = { "lua", "javascript", "c", "html", "cpp"},
 			auto_install = true,
       highlight = { enable = true },
 			indent = { enabel = true },
